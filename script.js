4fe439be47b091f5734b43c8caacb3fc9b35194c
Justin Lee Attinoto <justinattinoto@gmail.com>
2024-10-12 16:45:40 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Basic interactivity for adding items to the cart
-document.addEventListener('DOMContentLoaded', () => {
-    const buttons = document.querySelectorAll('.product button');
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            alert('Item added to cart!');
-        });
-    });
-});
